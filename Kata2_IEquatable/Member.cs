fe70a45ff7b63bf324b6fe6a5f5a627150eb0bee
Jonathan <Jonathan@DESKTOP-CKOU825>
2022-02-23 10:05:07 +0100

    Tried

---
+++ Kata2_IEquatable/Member.cs
@@ -0,0 +1,32 @@
+﻿using Kata2_IEquitable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata2_IEquatable
+{
+    internal class Member : IMember
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public MemberLevel Level { get; set; }
+        public DateTime Since { get; set; }
+
+        public int CompareTo(IMember? other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Equals(IMember? other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RandomInit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Kata2_IEquatable/MemberList.cs
@@ -0,0 +1,27 @@
+﻿using Kata2_IEquitable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata2_IEquatable
+{
+    internal class MemberList : IMemberList
+    {
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count(int year)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Sort()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
